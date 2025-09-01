e35e38a4cc08530660bc529a11c2e529f1417d41
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-09-02 00:23:16 +0330

    first commit

--- collab/urls.py
+++ collab/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "collab"
 
 urlpatterns = [
-    path("", views.collab_page, name="page"),     # صفحهٔ همکاری (GET)
+    path("", views.collab_page, name="collab-view"),     # صفحهٔ همکاری (GET)
     path("project/", views.submit_project, name="project"),  # POST فرم پروژه
     path("hire/", views.submit_hire, name="hire"),           # POST فرم استخدام
     path("thanks/", views.thanks, name="thanks"),            # صفحهٔ تشکر
