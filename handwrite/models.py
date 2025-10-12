f66b6c993596e59ea6c89307d61b46a927d60236
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-10-13 00:45:18 +0330

    services added

--- handwrite/models.py
+++ handwrite/models.py
@@ -37,7 +37,7 @@ class Snippet(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("handwrite:handwrite-view", kwargs={"slug": self.slug})
+        return reverse("handwrite:handwrite-view")
 
     def save(self, *args, **kwargs):
         if not self.slug:
