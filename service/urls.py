ded5cb831cad94bfdbbc49f3d3519318fd048a48
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-10-20 04:23:33 +0330

    first commit

--- service/urls.py
+++ service/urls.py
@@ -14,7 +14,7 @@ router.register(r'technologies', TechnologyViewSet, basename='technology')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('<int:pk>/', views.service_view, name='service-view-id'),
+    path('<int:id>/', views.service_view, name='service-view-id'),
     re_path(r'^(?P<slug>[-\w\u0600-\u06FF]+)/$', views.service_view, name='service-view'),
 
 ]
