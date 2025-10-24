f71b1c0a9d557b7037a4340b08578077374aac0c
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-10-24 11:08:44 +0330

    first commit

--- handwrite/sitemaps.py
+++ handwrite/sitemaps.py
@@ -8,10 +8,19 @@ class SnippetSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return Snippet.objects.all()  # اگر فیلد انتشار دارید
+        # None اضافه کنیم برای URL اصلی صفحه دست‌خط‌ها
+        return [None] + list(Snippet.objects.filter(is_published=True))
 
     def location(self, obj):
-        return reverse('handwrite:handwrite-view')
+        if obj is None:
+            # URL اصلی لیست Snippet
+            return reverse('handwrite:handwrite-view')
+        # URL منحصر به فرد برای هر Snippet با slug
+        return reverse('handwrite:snippet-with-popup', kwargs={'slug': obj.slug})
 
     def lastmod(self, obj):
+        if obj is None:
+            # آخرین بروزرسانی صفحه اصلی = آخرین Snippet منتشر شده
+            last_snippet = Snippet.objects.filter(is_published=True).order_by('-updated_at').first()
+            return last_snippet.updated_at if last_snippet else None
         return obj.updated_at if hasattr(obj, 'updated_at') else obj.created_at
