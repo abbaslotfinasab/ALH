5e1bc76180684114e9190f10c1dde0bbac7926d3
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-10-24 08:07:44 +0330

    first commit

--- experience/sitemaps.py
+++ experience/sitemaps.py
@@ -1,31 +1,18 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from experience.models import Experience
 
-from experience.models import Project, Experience
 
-
-class ExperienceSitemap(Sitemap):
-    changefreq = "monthly"
-    priority = 0.7
-
-    def items(self):
-        return Experience.objects.all()  # اگر فیلد انتشار دارید
-
-    def location(self, obj):
-        return reverse('experience:experience-view')
-
-    def lastmod(self, obj):
-        return obj.updated_at if hasattr(obj, 'updated_at') else obj.created_at
-
-class ProjectSitemap(Sitemap):
+class ExperienceQuerySitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.8
 
     def items(self):
-        return Project.objects.all()  # اگر فیلد انتشار دارید
+        return Experience.objects.all()
 
     def location(self, obj):
-        return reverse('experience:experience-view')
+        base_url = reverse('experience:experience-view')
+        return f"{base_url}?slug={obj.company.name.replace(' ', '-')}"
 
     def lastmod(self, obj):
-        return obj.updated_at if hasattr(obj, 'updated_at') else obj.created_at
+        return obj.updated_at
